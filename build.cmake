cfc2157bea1cb0e020782224c4237bd454a0e5cb
Wasim Abbas <abbas.wasim@gmail.com>
2022-10-26 13:21:10 +0100

    Comment out most of the sanitiser options, only leaving address for now

--- build.cmake
+++ build.cmake
@@ -88,20 +88,20 @@ function(build_options target_name extra_pedantic)
 
 	if (ROAR_BUILD_SANITIZED)
 	  target_compile_options(${target_name} PRIVATE
-		-fsanitize=memory
-		-fsanitize=thread
-		-fsanitize=undefined
-		-fno-sanitize-recover=all
-		-fsanitize=float-divide-by-zero
-		-fsanitize=float-cast-overflow
-		-fno-sanitize=null
-		-fno-sanitize=alignment
+		# -fsanitize=memory
+		# -fsanitize=thread
+		# -fsanitize=undefined
+		# -fno-sanitize-recover=all
+		# -fsanitize=float-divide-by-zero
+		# -fsanitize=float-cast-overflow
+		# -fno-sanitize=null
+		# -fno-sanitize=alignment
 		-fsanitize=address)
 
 	  target_link_options(${target_name} PRIVATE
-		-fsanitize=undefined
-		-fsanitize=memory
-		-fsanitize=thread
+		# -fsanitize=undefined
+		# -fsanitize=memory
+		# -fsanitize=thread
 		-fsanitize=address)
 	endif()
   endif()
